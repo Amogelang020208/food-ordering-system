38cc25993e2d4bfb2f21d698993912ddc602faa9
Amogelang020208 <amogelang020208@iclould.com>
2026-06-19 11:35:49 +0200

    feat: create generic response wrapper

--- food-ordering-system/src/main/java/response/Response.java
+++ food-ordering-system/src/main/java/response/Response.java
@@ -1,4 +1,4 @@
-package com.jumpstart.food_ordering_system.response;
+package response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
@@ -14,4 +14,29 @@ public class Response<T> {
     private String message;
     private T data;
     private LocalDateTime timestamp;
+
+    public static <T> Response<T> success(T data, String message) {
+        return Response.<T>builder()
+                .statusCode(200)
+                .message(message)
+                .data(data)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> Response<T> success(T data) {
+        return success(data, "Success");
+    }
+
+    public static <T> Response<T> error(int statusCode, String message) {
+        return Response.<T>builder()
+                .statusCode(statusCode)
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> Response<T> error(String message) {
+        return error(400, message);
+    }
 }
